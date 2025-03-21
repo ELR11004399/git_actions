416f05e8d0f7b17cd8a389b1e6daa9658adbdea9
ELR11004399 <lobato_resendiz@hotmail.com>
2025-03-21 15:09:51 -0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int result(a,b)
+{
+  int result = a + b;
+  ret result;
+}
+int main (void)
+{
+  int a = 0;
+  int b = 1;
+  int result = 0;
+  result = sum(a,b);
+ ret 0;
+}
+
